828b89b9bc412ecc203641660db8727ceaca7f41
殷和义 <1140195102@qq.com>
2019-07-18 21:11:46 +0800

    提交traps.c文件。

--- kernel/traps.c
+++ kernel/traps.c
@@ -100,4 +100,119 @@ static void die(char* str, long esp_ptr, long nr)
 		do_exit(11);
 }
 
+void do_double_fault(long esp, long error_code)
+{
+	die("double fault", esp, error_code);
+}
 
+void do_general_protection(long esp, long error_code)
+{
+	die("general protection", esp, error_code);
+}
+
+void do_divide_error(long esp, long error_code)
+{
+	die("divide error", esp, error_code);
+}
+
+void do_int3(long* esp, long error_code, 
+	     long fs, long es, long ds,
+	     long ebp, long esi, long edi,
+	     long edx, long ecx, long ebx, long eax)
+{
+	int tr;
+	__asm__("str %%eax"             // str汇编指令：store the current task register to specified operand.
+		:"=a" (tr)
+		:"0" (0));
+	printk("eax\tebx\tecx\tedx\n\r%8x\t%8x\t%8x\t%8x\n\r", eax, ebx, ecx, edx);
+	printk("esi\tedi\tebp\tesp\n\r%8x\t%8x\t%8x\t%8x\n\r", esi, edi, ebp, (long)esp);
+	printk("\n\rds\tes\tfs\ttr\n\r%4x\t%4x\t%4x\t%4x\n\r", ds, es, fs, tr);
+	printk("EIP: %8x\tCS:%4x\tEFLAGS:%8x\n\r", esp[0], esp[1], esp[2]);
+}
+
+void do_nmi(long esp, long error_code)
+{
+	die("nmi", esp, error_code);
+}
+
+void do_debug(long esp, long error_code)
+{
+	die("debug", esp, error_code);
+}
+
+void do_overflow(long esp, long error_code)
+{
+	die("overflow", esp, error_code);
+}
+
+void do_bounds(long esp, long error_code)
+{
+	die("bounds", esp, error_code);
+}
+
+void do_invalid_op(long esp, long error_code)
+{
+	die("invalid_op", esp, error_code);
+}
+void do_device_not_available(long esp, long error_code)
+{
+	die("device not available", esp, error_code);
+}
+void do_coprocessor_segment_overrun(long esp, long error_code)
+{
+	die("coprocessor segment overrun", esp, error_code);
+}
+void do_invalid_TSS(long esp, long error_code)
+{
+	die("invalid TSS", esp, error_code);
+}
+void do_segment_not_present(long esp, long error_code)
+{
+	die("segment not present", esp, error_code);
+}
+void do_stack_segment(long esp, long error_code)
+{
+	die("stack segment", esp, error_code);
+}
+
+void do_coprocessor_error(long esp, long error_code)
+{
+	die("coprocessor error", esp, error_code);
+}
+
+/** @brief 这是intel保留的中断号，如果用户使用了它，就会产生中断错误提示。 */
+void do_reserved(long esp, long error_code)
+{
+	die("reserved(15, 17-47) error", esp, error_code);
+}
+
+/** @brief 中断门的初始化函数， 会在main函数中被调用
+*/
+void trap_init(void)
+{
+	set_trap_gate(0, &divide_error);
+	set_trap_gate(1, &debug);
+	set_trap_gate(2, &nmi);
+	set_system_gate(3, &int3);
+	set_system_gate(4, &overflow);
+	set_system_gate(5, &bounds);
+	set_trap_gate(6, &invalid_op);
+	set_trap_gate(7, &device_not_available);
+	set_trap_gate(8, &double_fault);
+	set_trap_gate(9, &coprocessor_segment_overrun);
+	set_trap_gate(10, &invalid_TSS);
+	set_trap_gate(11, &segment_not_present);
+	set_trap_gate(12, &stack_segment);
+	set_trap_gate(13, &general_protection);
+	set_trap_gate(14, &page_fault);
+	set_trap_gate(15, &reserved);
+	set_trap_gate(16, &coprocessor_error);
+	for (i = 17; i < 48; ++i)
+		set_trap_gate(i, &reserved);
+	set_trap_gate(45, &irq13);
+	outb_p(inb_p(0x21) & 0xfb, 0x21);        //outb_p和inb_p是在io.h中定义的宏函数，用于实现读端口和写端口。
+	outb_p(inb_p(0xA1) & 0xdf, 0xA1);
+	set_trap_gate(39, parallel_interrupt);
+}
+
+	
