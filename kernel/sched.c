3b5c6993ae122578eda835ce49645d7f31367acb
殷和义 <1140195102@qq.com>
2019-07-20 14:25:01 +0800

    Create sched.c

---
+++ kernel/sched.c
@@ -0,0 +1,10 @@
+#include <linux/sched.h>
+#include <linux/kernel.h>
+#include <linux/sys.h>
+#include <linux/fdreg.h>
+#include <asm/system.h>
+#include <asm/io.h>
+#include <asm/segment.h>
+
+#include <signal.h>
+#define _S(nr) (1 << ((nr) - 1))
