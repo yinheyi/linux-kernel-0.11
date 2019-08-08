511991e101065eef2c4f576ac20c632f476a3fd2
殷和义 <1140195102@qq.com>
2019-08-08 19:07:27 +0800

    Update fork.c

--- kernel/fork.c
+++ kernel/fork.c
@@ -21,8 +21,8 @@ void verify_area(void* addr, int size)
 {
   unsigned long start = (unsigned long)addr;
   size += start & 0xfff;
-  start &= 0xfffff000; 
-  start += get_base(current->ldt[2]);
+  start &= 0xfffff000;                    // 此时的start是在进程的数据段内的偏移地址，
+  start += get_base(current->ldt[2]);     // 把start 加上 数据段的起始地址，得到了它整个线空间中的地址
   while (size > 0)
   {
     size -= 4096;
@@ -32,5 +32,32 @@ void verify_area(void* addr, int size)
 }
 
 /**
-*
+* @brief 该函数实现复制当前进程的task_struct到目的进程。
+* @param [in] nr 进程号
+* @param [in] p 目的进程的task_struct 结构体指针
+* @return 返回 int 类型，表示
 */
+int copy_mem(int nr, struct task_struct *p)
+{
+    unsigned long old_data_base;
+    unsigned long old_code_base;
+    unsigned long new_data_base;
+    unsigned long new_code_base;
+    unsigned long data_limit;
+    unsigned long code_limit;
+    
+    code_limit = get_limit(0x0f);    // 从0x0f选择子中获取段界限，0x0f其实就是局部描述符中的代码段。
+    data_limit = get_limit(0x17);    // 从0x17选择子中获取段界限，0x17其实就是局部描述符中的数据段。
+    old_code_base = get_base(current->ldt[1]);
+    old_data_base = get_base(current->ldt[2]);
+    
+    if (old_code_base != old_data_base)
+        painic(" 不支持分开的代码段与数据段。");
+    if (data_limit < code_limit)
+        panic("bad data_limit");
+    
+    // 从下面这行代码可以看出来，每一个进程占了64M(0x4000000) 的内存。
+    new_data_base = new_code_base = nr * 0x4000000;
+    
+    
+}
