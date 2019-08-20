9ece66fb4f6d2d9dea8b01166b59110b8be5d11d
殷和义 <1140195102@qq.com>
2019-08-20 19:55:13 +0800

    Update bitmap.c

--- fs/bitmap.c
+++ fs/bitmap.c
@@ -14,3 +14,22 @@
             "stosl"                                                 \
             ::"a" (0), "c" (BLOCK_SIZE / 4), "D" ((long)(addr))     \
             : "cx", "di")
+
+/**
+  @brief 定义了一个内嵌汇编谎言的宏函数，实现的功能是：对一个long类型变量的指定位设置为1，并且返回原来的值(1或0).
+  @param [in] nr 指定第几位。
+  @param [in] addr 要设计的变量的地址。
+  @return 返回0或1, 代表了你要设置的那个位原来的值。
+  
+  相关的汇编指令说明：  
+  - btsl src, dest  把src索引号指定的dest上的目的位复制到CF标志位上，并把目的位置为1.
+  - setb src  src为一个字节，如果CF位为1，则把src设置为1，如果CF位为0，则把src设置为0.
+  */
+#define set_bit(nr, addr) ({                                        \
+register int res __asm__("ax");                                     \
+__asm__ __volatile__("btsl %2, %3\n\t"                              \
+                     "setb %%al"                                    \
+                     :"=a"(res)                                     \
+                     :"0"(0), "r" (nr), "m" (*(addr)));             \
+res;})
+
