6669637b8716c656f3465e0cdd5009127f0e58f4
殷和义 <1140195102@qq.com>
2019-08-20 20:47:40 +0800

    Update bitmap.c

--- fs/bitmap.c
+++ fs/bitmap.c
@@ -33,3 +33,51 @@ __asm__ __volatile__("btsl %2, %3\n\t"                              \
                      :"0"(0), "r" (nr), "m" (*(addr)));             \
 res;})
 
+/**
+  @brief 定义了一个内嵌汇编谎言的宏函数，实现的功能是：对一个long类型变量的指定位设置为0，并且返回原来的值(1或0).
+  @param [in] nr 指定第几位。
+  @param [in] addr 要设计的变量的地址。
+  @return 返回0或1, 代表了你要设置的那个位原来的值。
+  
+  相关的汇编指令说明：  
+  - btrl src, dest  把src索引号指定的dest上的目的位复制到CF标志位上，并把目的位置为0.
+  - setb src  src为一个字节，如果CF位为1，则把src设置为1，如果CF位为0，则把src设置为0.
+  */
+#define clear_bit(nr, addr) ({                                      \
+register int res __asm__("ax");                                     \
+__asm__ __volatile__("btrl %2, %3\n\t"                              \
+                     "setb %%al"                                    \
+                     :"=a"(res)                                     \
+                     :"0"(0), "r" (nr), "m" (*(addr)));             \
+res;})
+
+/**
+  @brief 该宏函数实现了从给定地址处查找第一个为0的位，返回查找到的下标，如果没有查找到，则返回0.
+  该宏函数只查找从addr处偏移到1kb的内存空间。
+  @param [in] addr 给定的地址。
+  @return 返回第一个为0的位的下标。
+  
+  涉及到的汇编指令包含：  
+  - cld 指令：清空方向位DF， 使得一些字符串指令增加si和di的值。
+  - lodsl指令：load string，把ds:si地址处的一个long类型变量放到eax寄存器中。
+  - notl src 指令：对每一位进行逻辑
+  - bsfl src des 指令： bit scan forward, 从src中查找（由0位至n位)查找第一个为1的位。如果
+    查找到，则把ZF置1，并且把相应位的下标索引值放到des中；如果查找不到，ZF位则置为0.
+  */
+#define find_first_zero(addr) ({                                   \
+int __res;                                                         \
+__asm__("cld\n\t"                                                  \
+        "1: lodsl \n\t"                                            \
+        "notl %%eax\n\t"                                           \
+        "bsfl %%eax, %%edx\n\t"                                    \
+        "je 2f\n\t"                                                \
+        "addl %%edx, %%ecx\n\t"                                    \
+        "jmp 3f\n"                                                 \
+        "2: addl $32, %%ecx\n\t"                                   \
+        "cmpl $8192, %%ecx\n\t"                                    \
+        "jl 1b\n"                                                  \
+        "3:"                                                       \
+        :"=c"(__res)                                               \
+        :"c"(0), "S"(addr)                                         \
+        :"ax", "dx", "si");                                        \
+__res;})
