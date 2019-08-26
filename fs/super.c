7c4d68c10d96e125e04297f3c9af708957963560
殷和义 <1140195102@qq.com>
2019-08-26 18:34:42 +0800

    Update super.c

--- fs/super.c
+++ fs/super.c
@@ -25,7 +25,7 @@ __asm__("bt %2, %3\t\n"                                   \
         :"a" (0), "r" (bitnr), "m" (*(addr)));            \
 __res;})
 
-struct super_block super_block[NR_SUPER];     //共 8 项
+struct super_block super_blocks[NR_SUPER];     //共 8 项
 int ROOT_DEV = 0;
 
 /**
@@ -66,4 +66,68 @@ static void wait_on_super(struct super_block* sb)
     while (sb->s_lock)
         sleep_on(&(sb->s_wait));
     sti();
+}
+
+/**
+  @brief 该函数用于获取指定设备上的超级块，如果不存在，则返回NULL.
+  @param [in] dev 设备号
+  @return 返回超级块指针.
+  
+  该函数遍历内存中的超级块数组，查找与指定设备相同的超级块。
+  */
+struct super_block* get_super(int dev)
+{
+    struct super_block* s;
+    if (!dev)
+        return NULL;
+    s = 0 + super_blocks;
+    while (s < NR_SUPER + super_blocks)
+    {
+        if (s->s_dev == dev)
+        {
+            wait_on_super(s);
+            if (s->s_dev == dev)
+                return s;
+            s = super_blocks + 0;
+        }
+        else
+            ++s;
+    }
+    return NULL;
+}
+
+/**
+  @brief
+  @param
+  @return
+  */
+void put_super(int dev)
+{
+    struct super_block* sb;
+    struct m_inode* inode;
+    int i;
+    
+    if (dev == ROOT_DEV)
+    {
+        printk("root diskette changed: prepare for armageddon\n\r");
+        return;
+    }
+    if (!(sb = get_super(dev)))
+        return;
+    if (sb->s_imount)
+    {
+        printk("mounted disk changed -tssk, tssk\n\r");
+        return;
+    }
+    
+    lock_super(sb);
+    sb->s_dev = 0;
+    for (i = 0; i < I_MAP_SLOTS; ++i)
+        brelse(sb->s_imap[i]);
+    for (i = 0; i < Z_MAP_SLOT; ++i)
+        brelse(sb->s_zmap[i]);
+    free_super(sb);
+    return;
+    }
+    }
 }
