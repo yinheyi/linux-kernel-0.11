efa274af13d9566338c27f0b7e8d7f6cb18ee5c5
殷和义 <1140195102@qq.com>
2019-09-10 19:22:17 +0800

    完成file_write()函数。 。。

--- fs/file_dev.c
+++ fs/file_dev.c
@@ -10,7 +10,7 @@
 /**
   @brief 该函数实现读取一定字节数目的内容到用户的缓冲区中。
   @param [in] inode 一个文件的inode节点，里面有设备号的信息.
-  @param [in] filep 打开文件的指针，里面有当前文件当前指针的位置信息。
+  @param [in] filep 打开文件的指针，里面有当前文件中的当前指针的位置信息。
   @param [in] buf   用户的缓冲区
   @param [in] count 要读取的字节数。
   @return 返回成功读取的字节数， 如果为0，返回-ERROR.
@@ -56,4 +56,52 @@ int file_read(struct m_inode* inode, struct file* filp, char* buf, int count)
     inode->i_atime = CURRENT_TIME;
     return (count - left) ? (count - left) : -ERROR;
 }
+
+int file_write(struct m_inode* inode, struct file* filp, char* buf, int count)
+{
+    off_t pos;
+    int block, c;
+    struct buffer_head* bh;
+    char* p;
+    int i = 0;
+    
+    // 如果是append文件，则需要把pos的值设置为文件尾部。
+    if (filp->f_flag & O_APPEND)
+        pos = inode->i_size;
+    else
+        pos = filp->f_pos;
+    
+    while (i < count)
+    {
+        if (!(block = create_block(inode, pos / BLOCK_SIZE)))
+            break;
+        if (!(bh = bread(inode->i_dev, block)))
+            break;
+        
+        c = pos % BLOCK_SIZE;
+        p = c + bh->b_data;      // p 指向缓冲块内开始写入数据的位置
+        c = BLOCK - c;
+        if (c > count - i)       // c 表示当前缓冲块可以写入的字节数。
+            c = count - i;
+        pos += c;
+        if (pos > inode->i_size)    // pos 就是一个文件内的cursor，当指向的位置大于了文件的大小，就需要修改文件大小了。
+        {
+            inode->i_size = pos;
+            inode->i_dirt = 1;
+        }
+        
+        while (c-- > 0)
+            *(p++) = get_fs_byte(buf++);
+        i += c;
+        bh->b_dirt = 1;
+        brelse(bh);
+    }
+    inode->i_mtime = CURRENT_TIME;
+    // 为什么呢？为什么只有不是O_APPEND的时候，才会更新f_pos和inode的i_ctime呢？？？
+    if (!(filp->f_flags & O_APPEND))
+    {
+        filp->f_pos = pos;
+        inode->i_ctime = CUURENT_TIME;
+    }
+    return (i ? i : -1);
 }
